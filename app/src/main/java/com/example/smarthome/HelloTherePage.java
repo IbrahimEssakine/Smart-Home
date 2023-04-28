0cf4f3af2b99545604a34e87835d880de2bff053
Mouad4399 <mouad.aitougrram@usms.ac.ma>
2023-04-28 20:16:59 +0100

    Building ButoomSheet Login form

--- app/src/main/java/com/example/smarthome/HelloTherePage.java
+++ app/src/main/java/com/example/smarthome/HelloTherePage.java
@@ -2,19 +2,71 @@ package com.example.smarthome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.transition.ChangeBounds;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
 
 import pl.droidsonroids.gif.GifDrawable;
 import pl.droidsonroids.gif.GifImageView;
 
 public class HelloTherePage extends AppCompatActivity {
 
+
+    Button loginbutton ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        ChangeBounds bounds = new ChangeBounds();
+        bounds.setDuration(500);
+        getWindow().setSharedElementEnterTransition(bounds);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_there_page);
 
         GifImageView gif = (GifImageView)findViewById(R.id.LogoGif);
         ((GifDrawable)gif.getDrawable()).stop();
+
+        loginbutton = findViewById(R.id.button_login);
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+
+            private void showDialog() {
+                final Dialog dialog = new Dialog(HelloTherePage.this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.login_buttomsheet);
+
+                Button Continuewithfirebase = dialog.findViewById(R.id.buttoncontinue);
+                Continuewithfirebase.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(HelloTherePage.this, "Redirecting You To Firebase Account", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                dialog.show();
+                dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+                dialog.getWindow().setGravity(Gravity.BOTTOM);
+
+            }
+        });
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/smarthome/MainActivity.java
+++ app/src/main/java/com/example/smarthome/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.smarthome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
