746db5e84eb198dbdc345a3b92e103646040c701
IBRAHIM ESSAKINE <103626975+IbrahimEssakine@users.noreply.github.com>
2023-05-11 15:15:55 +0100

    git init back end google acc create acc create house

---
+++ app/src/main/java/com/example/smarthome/CreateHousePage.java
@@ -0,0 +1,87 @@
+package com.example.smarthome;
+
+import androidx.annotation.NonNull;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.annotations.NotNull;
+import com.google.firebase.firestore.AggregateQuery;
+import com.google.firebase.firestore.AggregateSource;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.ktx.Firebase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class CreateHousePage extends AppCompatActivity {
+    TextView HName,Password,HexCode;
+    Button HouseCreation;
+    FirebaseFirestore firestore;
+    DatabaseReference database;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_house_page);
+        Bundle bundle = getIntent().getExtras();
+        ArrayList<String> user_data_list = bundle.getStringArrayList("UserDataListChange");
+        HName = findViewById(R.id.HouseTxt);
+        Password = findViewById(R.id.KeypadPassword);
+        HexCode = findViewById(R.id.CardHexNbr);
+        HouseCreation= findViewById(R.id.HouseCreation);
+        HouseCreation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                firestore=FirebaseFirestore.getInstance();
+
+                Map<String, Object> house_data = new HashMap<>();
+                house_data.put("Name", HName.getText().toString());
+                house_data.put("Password", Password.getText().toString());
+                house_data.put("HexCode", Arrays.asList(HexCode.getText().toString()));
+
+
+                firestore.collection("House")
+                        .add(house_data)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference HouseID) {
+                                //Toast.makeText(CreateHousePage.this,"DocumentSnapshot written with ID: " + documentReference.getId(),Toast.LENGTH_SHORT).show();
+                                Map<String, Object> user_data = new HashMap<>();
+                                user_data.put("Name", user_data_list.get(0));
+                                user_data.put("Last Name",user_data_list.get(1));
+                                user_data.put("Email", user_data_list.get(2));
+                                user_data.put("Houses", Arrays.asList(HouseID.getId()));
+                                firestore.collection("user").add(user_data);
+
+                                database = FirebaseDatabase.getInstance().getReference (HouseID.getId());
+                                //DatabaseReference usersRef = database.child("Sensor");
+                                //Map<String,Object> Sensor = new HashMap<>();
+                                //Sensor.put("LED", Arrays.asList(1,2,3));
+                                //usersRef.setValue(Sensor);
+                                DatabaseReference usersRefPass = database.child("Password");
+                                usersRefPass.setValue(Password.getText().toString());
+                            }
+                        });
+
+
+
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/smarthome/SignUpPage.java
+++ app/src/main/java/com/example/smarthome/SignUpPage.java
@@ -1,20 +1,43 @@
-package com.example.smarthome;
+    package com.example.smarthome;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class SignUpPage extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.AggregateQuery;
+import com.google.firebase.firestore.AggregateQuerySnapshot;
+import com.google.firebase.firestore.AggregateSource;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
+import java.util.ArrayList;
+
+
+    public class SignUpPage extends AppCompatActivity {
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
     Button returnbackbutton ;
+
+    FirebaseFirestore firestore;
+    ArrayList<String> data_user_list=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
-
 
         returnbackbutton = findViewById(R.id.buttonback);
         returnbackbutton.setOnClickListener(new View.OnClickListener() {
@@ -25,19 +48,76 @@ public class SignUpPage extends AppCompatActivity {
         });
 
         Button creataccfirebase = findViewById(R.id.buttoncreatfirebaseacc);
+        gso=new GoogleSignInOptions. Builder (GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        gsc = GoogleSignIn.getClient(this,gso);
+
         creataccfirebase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(SignUpPage.this, "Redirecting You To Firebase", Toast.LENGTH_SHORT).show();
+                SignIn();
             }
         });
 
 
     }
 
+    private void SignIn() {
+        Intent intent=gsc.getSignInIntent();
+        startActivityForResult(intent, 100);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult (requestCode, resultCode, data);
+        if (requestCode==100) {
+            Task<GoogleSignInAccount> task=GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                task.getResult (ApiException.class);
+//                finish();
+                GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+                if (account!=null) {
+                    String Name=account.getGivenName();
+                    String Family=account.getFamilyName();
+                    String Mail=account.getEmail();
+                    firestore=FirebaseFirestore.getInstance();
+
+
+                    AggregateQuery countQuery = firestore.collection ("user").whereEqualTo("Email",Mail).count();
+                    countQuery.get(AggregateSource.SERVER).addOnCompleteListener(tasks -> {
+                    AggregateQuerySnapshot snapshot = tasks.getResult();
+
+                        if(snapshot.getCount()!=0){
+                            Toast.makeText(SignUpPage.this, "this acc exist  "+snapshot.getCount(), Toast.LENGTH_SHORT).show();
+                            gsc.signOut();
+                            SignIn();
+                        }else{
+                            data_user_list.add(0,Name);
+                            data_user_list.add(1,Family);
+                            data_user_list.add(2,Mail);
+
+                            Toast.makeText( this, "Email : "+Mail+" Name : "+Name, Toast.LENGTH_SHORT).show();
+                            HomeActivity();
+                        }
+                    });
+                }
+
+            } catch (ApiException e) {
+                Toast.makeText( this, "hada errir", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
     @Override
     public void finish() {
         super.finish();
         overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
+    }
+    private void HomeActivity() {
+        finish();
+        Intent intent=new Intent(getApplicationContext(), HomePage.class);
+        intent.putExtra("UserDataList",data_user_list);
+        startActivity (intent);
     }
 }
