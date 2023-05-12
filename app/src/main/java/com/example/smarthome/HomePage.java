c70e1fed3e8acf0eacffb84206c5e8a5da9650b5
IBRAHIM ESSAKINE <103626975+IbrahimEssakine@users.noreply.github.com>
2023-05-12 17:42:52 +0100

    homepage java

--- app/src/main/java/com/example/smarthome/HomePage.java
+++ app/src/main/java/com/example/smarthome/HomePage.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
+
 import java.util.ArrayList;
 public class HomePage extends AppCompatActivity {
-    TextView UsernameTxt, PasswordTxt,PhoneNbr,EmailTxt;
+    TextInputLayout UsernameTxt, PasswordTxt,PhoneNbr;
+    TextView EmailTxt;
     ArrayList<String>data_user=new ArrayList<>();
     Button CreateHouse;
     @Override
@@ -24,16 +28,16 @@ public class HomePage extends AppCompatActivity {
         EmailTxt = findViewById(R.id.EmailTxt);
         CreateHouse= findViewById(R.id.CreateHouse);
 
-        UsernameTxt.setText(user_data_list.get(0));
+        UsernameTxt.getEditText().setText(user_data_list.get(0));
         EmailTxt.setText(user_data_list.get(1));
 
         CreateHouse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                data_user.add(0,UsernameTxt.getText().toString());
-                data_user.add(1,PhoneNbr.getText().toString());
+                data_user.add(0,UsernameTxt.getEditText().getText().toString());
+                data_user.add(1,PhoneNbr.getEditText().getText().toString());
                 data_user.add(2,EmailTxt.getText().toString());
-                data_user.add(3,PasswordTxt.getText().toString());
+                data_user.add(3,PasswordTxt.getEditText().getText().toString());
                 Intent intent=new Intent(getApplicationContext(), CreateHousePage.class);
                 intent.putExtra("UserDataListChange",data_user);
                 finish();
